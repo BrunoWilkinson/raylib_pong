4c17766f1ac40b625a7fedb4d9b2df9662ff1925
BrunoWilkinson <bruno.wilkinson@gmail.com>
2026-03-28 19:02:58 +0000

    setup

--- sources/main.c
+++ sources/main.c
@@ -22,7 +22,7 @@ int main(void)
         const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
         DrawTexture(texture, texture_x, texture_y, WHITE);
 
-        const char* text = "OMG! IT WORKS!";
+        const char* text = "Hello World!";
         const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
         DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
